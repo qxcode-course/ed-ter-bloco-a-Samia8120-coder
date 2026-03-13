ce493f29b1be392b430b4003e8c5fed04d561f82
Samia8120-coder <medusa8120@gmail.com>
2026-03-13 11:08:39 +0000

    Finalização da atividade Pintando

--- ed/pintando/go/draft.go
+++ ed/pintando/go/draft.go
@@ -9,13 +9,13 @@ func main() {
 	var n1 float64
 	var n2 float64
 	var n3 float64
-	fmt.Scanf("%f %f %f", n1, n2, n3)
-	//fmt.Scanln(n2)
+	//fmt.Scanf("%f %f %f", n1, n2, n3)
+	fmt.Scan(&n1, &n2, &n3)
 	//fmt.Scanln(n3)
 
 	p := (n1 + n2 + n3) / 2
 	//temp := p * (p - n1) * (p - n2) * (p - n3)
-	res := math.Sqrt((p * (p - n1*(p-n2)*(p-n3))))
+	res := math.Sqrt((p * (p - n1) * (p - n2) * (p - n3)))
 
-	fmt.Print(res)
+	fmt.Printf("%.2f\n", res)
 }
