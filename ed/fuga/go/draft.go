e815b5c78fa99a499379d2889d99c2a7dbc0a89c
Samia8120-coder <medusa8120@gmail.com>
2026-03-18 21:30:30 +0000

    Finalização da atividade Fuga

--- ed/fuga/go/draft.go
+++ ed/fuga/go/draft.go
@@ -1,5 +1,41 @@
 package main
+
 import "fmt"
+
+func caminho(h int, p int, f int, d int) {
+	if d == 1 {
+		for f <= 16 {
+			f++
+			if f == 16 {
+				f = 0
+			}
+			if f == p {
+				fmt.Print("N\n")
+				break
+			} else if f == h {
+				fmt.Print("S\n")
+				break
+			}
+		}
+	} else if d == -1 {
+		for f < 16 {
+			f--
+			if f == -1 {
+				f = 15
+			}
+			if f == p {
+				fmt.Print("N\n")
+				break
+			} else if f == h {
+				fmt.Print("S\n")
+				break
+			}
+		}
+	}
+}
 func main() {
-    fmt.Println("qxcode")
+	var H, P, F, D int
+	fmt.Scan(&H, &P, &F, &D)
+
+	caminho(H, P, F, D)
 }
