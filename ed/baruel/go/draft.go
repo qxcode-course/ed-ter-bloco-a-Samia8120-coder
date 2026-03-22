0b74e4c9012f437167213f60279800146c2b9092
Samia8120-coder <medusa8120@gmail.com>
2026-03-22 18:41:02 +0000

    Finalização da atividade Baruel

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -1,10 +1,38 @@
 package main
-
+import "fmt"
 func main() {
-	var totalfigalbum int
-	var totalbaruel int
+	var qtd_album, qtd_possui int
+	fmt.Scan(&qtd_album, &qtd_possui)
+	figuras := make([]int, qtd_possui)
+	for i := range figuras{
+		fmt.Scan(&figuras[i])
+	}
 
-	var sequencia[] int
-	for i := range 
-	
+	unicos := make(map[int]bool)
+	repetidos := make([]int, 0, qtd_possui)
+	for _, figura := range figuras{
+		if unicos[figura] {
+			repetidos = append(repetidos, figura)
+		} else {
+			unicos[figura] = true
+		}
+	}
+	saida := fmt.Sprintf("%v", repetidos)
+	if saida == "[]" {
+		fmt.Println("N")
+	} else {
+	fmt.Println(saida[1: len(saida)-1])
+	}
+	faltantes := make([]int, 0, qtd_album)
+	for i := 1; i <= qtd_album; i++{
+		if !unicos[i]{
+			faltantes = append(faltantes, i)
+		}
+	}
+	saida = fmt.Sprintf("%v", faltantes)
+	if saida == "[]" {
+		fmt.Println("N")
+	} else {
+	fmt.Println(saida[1: len(saida)-1])
+	}
 }
