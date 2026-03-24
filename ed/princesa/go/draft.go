37b159cb4a59d3a538a3c9a78d154ba43df47e9e
Samia8120-coder <medusa8120@gmail.com>
2026-03-24 14:26:53 +0000

    salvando

---
+++ ed/princesa/go/draft.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main() {
+    var tamanho, posicao int
+    fmt.Scan(&tamanho, &posicao)
+    
+    vet_elementos := make([]int, tamanho)
+    vet_vivos := make([]int, posicao)
+    
+    if {
+
+    }
+
+}
