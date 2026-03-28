e048ed85a73f52a275be4e6e348fd5ce1ae3da39
Samia8120-coder <medusa8120@gmail.com>
2026-03-28 23:06:43 +0000

    tentiva da atividade princesa v1

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,14 +1,30 @@
 package main
 import "fmt"
+
+type Soldado struct{
+    vivo, espada int
+}
+
+func matar(){
+    //mata ai
+}
+func cacar_vivo(){
+    //caça vivo e da a espada
+}
 func main() {
     var tamanho, posicao int
     fmt.Scan(&tamanho, &posicao)
-    
-    vet_elementos := make([]int, tamanho)
-    vet_vivos := make([]int, posicao)
-    
-    if {
+    vet := make([]Soldado, tamanho)
+    for i := range tamanho {
+        fmt.Scan(vet[i])
+    }
 
+    ax := 0
+    for ax > 1{
+        //coisos
+        for {
+            
+        }
     }
 
 }
