539945bc99ddac9e8cf854b5c084182df177b986
Samia8120-coder <medusa8120@gmail.com>
2026-03-31 00:23:42 +0000

    Finalizacao da atividade busca

--- ed/buscas/go/draft.go
+++ ed/buscas/go/draft.go
@@ -2,27 +2,39 @@ package main
 
 import "fmt"
 
-func matchingStrings (t1 int, t2 int) {
-	fmt.Scan(&t1)
-	//palavras := [][]any{}
-	vetor_consulta := make(map[int]string)
-	for i := 0; i < t1; i++ {
-		var palavra string
-		fmt.Scan(&palavra)
-		vetor_consulta[i] = palavra
+func matchingStrings (consulta []string, busca []string) []int{
+	mapa := make(map[string]int)
+	for _, palavra := range consulta {
+		mapa[palavra]++
 	}
+	juncao := make([]int, len(busca))
+	for i, palavra := range busca {
+		juncao[i] = mapa[palavra]
+	}
+	return juncao
+}
 
+func main() {
+	//entrada
+	var t1, t2 int
+		fmt.Scan(&t1)
+	vetor_consulta := make([]string, t1)
+	for i := 0; i < t1; i++ {
+		fmt.Scan(&vetor_consulta[i])
+	}
 	fmt.Scan(&t2)
 	vetor_busca := make([]string, t2)
 	for i := 0; i < t2; i++ {
 		fmt.Scan(&vetor_busca[i])
 	}
 
-}
-
-func main() {
-	//entrada
-	var t1, t2 int
-	matchingStrings(t1, t2)
-
+	juncao := matchingStrings(vetor_consulta, vetor_busca)
+	for i, qtd := range juncao {
+		if i == len(juncao)-1{
+			fmt.Printf("%d", qtd)
+			break
+		}
+		fmt.Printf("%d ", qtd)
+	}
+	fmt.Println()
 }
