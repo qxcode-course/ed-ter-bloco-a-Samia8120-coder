71795c602357f19638b084d10c5ae0d885af83fd
Samia8120-coder <medusa8120@gmail.com>
2026-04-08 00:59:05 +0000

    Finalização da atividade Resto

--- ed/resto/go/draft.go
+++ ed/resto/go/draft.go
@@ -1,34 +1,19 @@
 package main
 import "fmt"
-//eu boto cada divisao aonde??
-func div(num int) []int{
-    //vet := make([]int, 0)
-    mapa := make(map[int]int)
-    var resto, inteiro int
-    for {
-        if num < 0 {
-            break
-        }else{
-            mapa[]
-        }
+
+func div(num int){
+    if num == 0 {
+        return
     }
-    /*for i := range vetor {
-        f = vetor[i] / 2
-    }
-    for _, resto := range vetor{
-        mapa[f] = resto % 2
-    }
-    vet := make([]int, len(vetor))
-    for i, resto := range vetor {
-        vet[i] = mapa[resto]
-    }*/
-    return vet
+    quo := num / 2
+    resto := num % 2
+
+    div(quo)
+    fmt.Println(quo, resto)
 }
 
 func main() {
     var num int
     fmt.Scan(&num)
-    //vetor := make([]int, num)
-    fmt.Print(div(num))
-    //contrario(vetor)
+    div(num)
 }
