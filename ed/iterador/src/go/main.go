80863cd4dc387533d9f152470c36056c1a03d770
Samia8120-coder <medusa8120@gmail.com>
2026-05-03 00:51:39 +0000

    Duas Atividades Finalizadas

---
+++ ed/iterador/src/go/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type MyList struct {
+	data []int
+}
+
+type Iterator struct {
+	data  []int
+	index int
+}
+
+type CyclicIterator struct {
+	data  []int
+	index int
+}
+
+func NewMyList(values []int) *MyList {
+	return &MyList{data: values}
+}
+
+func (l *MyList) Iterator() *Iterator {
+	return &Iterator{data: l.data, index: -1}
+}
+
+func (i *Iterator) HasNext() bool {
+	return i.index < len(i.data)-1
+}
+
+func (i *Iterator) Next() int {
+	if i.index == len(i.data) {
+		panic(fmt.Errorf("No more elements"))
+	}
+	i.index += 1
+	return i.data[i.index]
+}
+
+func (l *MyList) ReverseIterator() *Iterator {
+	rev := make([]int, len(l.data))
+	for i := 0; i < len(l.data); i++ {
+		rev[i] = l.data[len(l.data)-1-i]
+	}
+	return &Iterator{data: rev, index: -1}
+}
+
+func (l *MyList) CyclicIterator() *CyclicIterator {
+	return &CyclicIterator{data: l.data, index: -1}
+}
+
+func (c *CyclicIterator) Next() int {
+	if len(c.data) == 0 {
+		panic(fmt.Errorf("Empty list"))
+	}
+	c.index = (c.index + 1) % len(c.data)
+	return c.data[c.index]
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	mylist := NewMyList([]int{})
+	for scanner.Scan() {
+		line := scanner.Text()
+		args := strings.Fields(line)
+		fmt.Println("$" + line)
+
+		switch args[0] {
+		case "end":
+			break
+		case "read":
+			for i := 1; i < len(args); i++ {
+				slice := make([]int, len(args)-1)
+				for i, value := range args[1:] {
+					slice[i], _ = strconv.Atoi(value)
+				}
+				mylist = NewMyList(slice)
+			}
+		case "show":
+			fmt.Print("[ ")
+			for it := mylist.Iterator(); it.HasNext(); {
+				fmt.Printf("%v ", it.Next())
+			}
+			fmt.Println("]")
+		case "reverse":
+			fmt.Print("[ ")
+			for it := mylist.ReverseIterator(); it.HasNext(); {
+				fmt.Printf("%v ", it.Next())
+			}
+			fmt.Println("]")
+		case "cyclic":
+			qtd, _ := strconv.Atoi(args[1])
+			fmt.Print("[ ")
+			it := mylist.CyclicIterator()
+			for range qtd {
+				fmt.Printf("%v ", it.Next())
+			}
+			fmt.Println("]")
+		}
+	}
+
+}
